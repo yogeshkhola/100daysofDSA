97b800ebe75130ff1493a3fb5fed968e9fb3c627
yogeshkhola <79782579+yogeshkhola@users.noreply.github.com>
2021-03-01 11:35:43 +0530

    data structure

---
+++ day 1/array.py
@@ -0,0 +1,25 @@
+# Python program for array rotation(left shift) by 2 elements
+
+arr=[1,2,3,4,5,6,7]
+
+# function to left rotate the array[] of size n by d
+def leftRotate(arr,d,n):
+    for i in range(d):
+        leftRotatebyOne(arr,n)
+
+#function to left rotate arr[] by size 1
+
+def leftRotatebyOne(arr,n):
+    temp=arr[0]
+    for i in range(n-1):
+        arr[i]=arr[i+1]
+
+    arr[n-1]=temp
+
+def printarray(arr,size):
+    for i in range(size):
+        print(" %d" % arr[i],end="")
+
+
+leftRotate(arr,2,7)
+printarray(arr,7)
