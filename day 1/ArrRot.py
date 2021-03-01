1a0c4ef6d17bfa7081fc54b8b7837dfe9e61d9f7
yogeshkhola <79782579+yogeshkhola@users.noreply.github.com>
2021-03-01 23:13:39 +0530

    dsa

--- day 1/ArrRot.py
+++ day 1/ArrRot.py
@@ -5,7 +5,7 @@ for i in range(0,shift):
     temp=array[0]
 
     for j in range(0,len(array)-1):
-        array[j]=array[j+1]
+        array[j]=array[j+1] # shift all elements toward left
     array[len(array)-1] =temp
 
 for i in range (0,len(array)):
---
+++ day 1/RightRotaion.py
@@ -0,0 +1,20 @@
+num=[1,2,3,4,5]
+shift=2
+def rightArrayRotation(num_array,shift):
+    for i in range(0,shift):
+        temp=num_array[len(num_array)-1]
+        for j in range(len(num_array)-1,0,-1):
+            num_array[j]=num_array[j-1]#right shift
+        num_array[0]=temp
+    return num_array
+
+def printArray(array):
+    for i in range(0,len(array)):
+        print(array[i],end=" ")
+
+print("Array before rotation")
+printArray(num)
+
+rotated_array=rightArrayRotation(num,shift)
+print("\n Array after right rotation")
+printArray(rotated_array)
