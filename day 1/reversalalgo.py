7c4466e6a591799719a59865838fee2e696f851d
yogeshkhola <79782579+yogeshkhola@users.noreply.github.com>
2021-03-01 23:19:42 +0530

    DSA

--- day 1/reversalalgo.py
+++ day 1/reversalalgo.py
@@ -1,9 +1,9 @@
 # Python program for reversal algorithm of array rotation
 
-# Function to reverse arr[] from index start to end
+a = [1, 2, 3, 4, 5, 6, 7, 8]
 
 
-def reverseArray(arr, start, end):
+def reverse_array(arr, start, end):
     while (start < end):
         temp = arr[start]
         arr[start] = arr[end]
@@ -12,35 +12,7 @@ def reverseArray(arr, start, end):
         end = end - 1
 
 
-# Function to left rotate arr[] of size n by d
-
-
-def leftRotate(arr, d):
-    if d == 0:
-        return
-    n = len(arr)
-    # in case the rotating factor is
-    # greater than array length
-    d = d % n
-    reverseArray(arr, 0, d - 1)
-    reverseArray(arr, d, n - 1)
-    reverseArray(arr, 0, n - 1)
-
-
-# Function to print an array
-
-
-def printArray(arr):
-    for i in range(0, len(arr)):
-        print
-        arr[i],
-
-    # Driver function to test above functions
-
-
-arr = [1, 2, 3, 4, 5, 6, 7]
-n = len(arr)
-d = 2
-
-leftRotate(arr, d)  # Rotate array by 2
-printArray(arr)
+reverse_array(a, 0, 1)
+reverse_array(a, 2, 7)
+reverse_array(a, 0, 7)
+print(a)
