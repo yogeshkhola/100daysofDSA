06dc72b0b70463616c5f098e207f984c1cea7652
yogeshkhola <79782579+yogeshkhola@users.noreply.github.com>
2021-03-03 22:00:36 +0530

    ..

---
+++ day4/linearSearch.py
@@ -0,0 +1,18 @@
+#python program for linear search
+
+def search(arr, n, x):
+	for i in range(0, n):
+		if (arr[i] == x):
+			return i
+	return -1
+
+arr = [2, 3, 4, 10, 40]
+x = 10
+n = len(arr)
+
+# Function call
+result = search(arr, n, x)
+if(result == -1):
+	print("Element is not present in array")
+else:
+	print("Element is present at index", result)
